0a26236dafb66e9fcbcb2bef4ae59c0d8259e552
pmarqu05 <pmarqu05@ucm.es>
2026-03-10 17:33:50 +0100

    crear vivienda corregido Pablo

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,9 +1,11 @@
+
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.RequestParam; 
 
 @Controller
 public class MyController {
@@ -13,10 +15,18 @@ public class MyController {
         return "index";
     }
 
+    // Le decimos que espere recibir el parámetro "nombreCasa" del formulario
     @PostMapping("/submit")
-    public String submit() {
-        System.out.println("Button clicked!");
-        new Dot();
+    public String submit(@RequestParam("nombreCasa") String nombreCasa, Model model) {
+        
+        System.out.println("El usuario ha creado la casa: " + nombreCasa);
+        
+        // 1. Instanciamos la clase extra simulando que lo preparamos para la Base de Datos
+        Vivienda nuevaVivienda = new Vivienda(nombreCasa);
+        
+        // 2. Le pasamos el nombre guardado en la clase Java a la vista (result.html)
+        model.addAttribute("nombreDeLaCasa", nuevaVivienda.getNombre());
+        
         return "result";
     }
 }
