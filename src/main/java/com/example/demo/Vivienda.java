e5a5f8b156cc0839992998aa5968687c11a7bd31
javivelascoo <javvelas@ucm.es>
2026-03-10 23:10:31 +0100

    HU1 Crear vivienda Velasco

--- src/main/java/com/example/demo/Vivienda.java
+++ src/main/java/com/example/demo/Vivienda.java
@@ -15,10 +15,8 @@ public class Vivienda {
         this.nombre = nombre;
     }
 
-    // Este es el método para "contentar" a tu compañero mañana
+    
     public void guardarSimulacionBD() {
-        // En el futuro, aquí el controlador llamará a un ViviendaRepository.
-        // Por ahora, simulamos por consola que hace algo.
         System.out.println("==== SIMULACIÓN DE BASE DE DATOS ====");
         System.out.println("Ejecutando INSERT INTO viviendas (nombre) VALUES ('" + this.nombre + "');");
         System.out.println("=====================================");
