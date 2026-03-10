6cdb9ce784e33643344eee29e344d6754cdf9095
GlymL <rdolera@ucm.es>
2026-03-10 18:55:16 +0100

    Gabi HU1

--- src/main/java/com/example/demo/MyController.java
+++ src/main/java/com/example/demo/MyController.java
@@ -1,12 +1,19 @@
 package com.example.demo;
 
+import java.util.HashSet;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
 @Controller
 public class MyController {
+
+    private HashSet<String> nombreCasas = new HashSet<String>();
 
     @GetMapping("/")
     public String home() {
@@ -14,9 +21,26 @@ public class MyController {
     }
 
     @PostMapping("/submit")
-    public String submit() {
-        System.out.println("Button clicked!");
-        new Dot();
+    public String submitHouse(
+            @RequestParam String houseName,
+            @RequestParam String description,
+            @RequestParam MultipartFile image,
+            RedirectAttributes redirectAttributes) {
+
+
+        if (nombreCasas.contains(houseName)) {
+
+            redirectAttributes.addFlashAttribute("errorMessage",
+                    "El nombre de una casa no puede existir ya, por favor, introduzca uno nuevo.");
+
+            return "redirect:/";
+        }
+        nombreCasas.add(houseName);
+        return "redirect:/result";
+    }
+
+    @GetMapping("/result")
+    public String resultPage() {
         return "result";
     }
 }
