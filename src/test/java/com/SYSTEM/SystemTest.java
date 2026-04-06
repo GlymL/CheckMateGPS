4be8f24ae3b45f81647d5c9f95234e1ba18d367d
infinite-alex <infinite.alex5@gmail.com>
2026-04-06 10:20:05 +0200

    in progress cm5 pruebas de sistema

--- src/test/java/com/SYSTEM/SystemTest.java
+++ src/test/java/com/SYSTEM/SystemTest.java
@@ -16,6 +16,9 @@ import com.example.demo.DemoApplication;
 import com.example.demo.RoommateRepository;
 import com.example.demo.Vivienda;
 import com.example.demo.ViviendaRepository;
+import com.example.demo.Tarea;
+import com.example.demo.TareaRepository;
+import com.example.demo.Roommate;
 
 @SpringBootTest(classes = DemoApplication.class)
 @AutoConfigureMockMvc
@@ -24,6 +27,9 @@ class SystemTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private TareaRepository tareaRepository;
 
     @Autowired
     private ViviendaRepository viviendaRepository;
@@ -47,5 +53,87 @@ class SystemTest {
                 .existsByNombreRealAndVivienda("Ana", vivienda);
 
         assertThat(exista).isTrue();
+    }
+
+    // por CM5
+    @Test
+    void assignTask_ok() throws Exception {
+
+        Vivienda vivienda = new Vivienda();
+        vivienda.setName("CasaTest");
+        viviendaRepository.save(vivienda);
+
+        Roommate roommate = new Roommate("user1", "Ana", vivienda);
+        roommateRepository.save(roommate);
+
+        Tarea tarea = new Tarea();
+        tarea.setName("Limpiar");
+        tarea.setVivienda(vivienda);
+        tareaRepository.save(tarea);
+
+        mockMvc.perform(post("/assignTask/submit")
+                .param("taskId", tarea.getId().toString())
+                .param("roommateId", roommate.getId().toString())
+                .param("viviendaId", vivienda.getId().toString()))
+            .andDo(print())
+            .andExpect(status().is3xxRedirection());
+
+        Tarea actualizada = tareaRepository.findById(tarea.getId()).get();
+        assertThat(actualizada.getRoommateId()).isEqualTo(roommate.getId().toString());
+    }
+    @Test
+    void assignTask_sinTask() throws Exception {
+
+        Vivienda vivienda = new Vivienda();
+        vivienda.setName("CasaTest");
+        viviendaRepository.save(vivienda);
+
+        Roommate roommate = new Roommate("user1", "Ana", vivienda);
+        roommateRepository.save(roommate);
+
+        mockMvc.perform(post("/assignTask/submit")
+                .param("roommateId", roommate.getId().toString())
+                .param("viviendaId", vivienda.getId().toString()))
+            .andExpect(status().is3xxRedirection());
+    }
+    @Test
+    void assignTask_sinRoommate() throws Exception {
+
+        Vivienda vivienda = new Vivienda();
+        vivienda.setName("CasaTest");
+        viviendaRepository.save(vivienda);
+
+        Tarea tarea = new Tarea();
+        tarea.setName("Limpiar");
+        tarea.setVivienda(vivienda);
+        tareaRepository.save(tarea);
+
+        mockMvc.perform(post("/assignTask/submit")
+                .param("taskId", tarea.getId().toString())
+                .param("viviendaId", vivienda.getId().toString()))
+            .andExpect(status().is3xxRedirection());
+    }
+    @Test
+    void assignTask_sinDatos() throws Exception {
+
+        mockMvc.perform(post("/assignTask/submit")
+                .param("viviendaId", "1"))
+            .andExpect(status().is3xxRedirection());
+    }
+    @Test
+    void assignTask_taskNoExiste() throws Exception {
+
+        Vivienda vivienda = new Vivienda();
+        vivienda.setName("CasaTest");
+        viviendaRepository.save(vivienda);
+
+        Roommate roommate = new Roommate("user1", "Ana", vivienda);
+        roommateRepository.save(roommate);
+
+        mockMvc.perform(post("/assignTask/submit")
+                .param("taskId", "999")
+                .param("roommateId", roommate.getId().toString())
+                .param("viviendaId", vivienda.getId().toString()))
+            .andExpect(status().is3xxRedirection());
     }
 }
