86822bc9e8c65d006c958656b201e0ecc006e4a1
pmarqu05 <pmarqu05@ucm.es>
2026-04-07 14:21:22 +0200

    Pruebas de aceptacion CM1

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -5,3 +5,14 @@ function openMenu(){
 function closeMenu(){
     document.getElementById("houseMenu").style.display="none";
 }
+
+function validarDescripcion(textarea) {
+    var regex = /^[a-zA-Z0-9 áéíóúÁÉÍÓÚñÑ.,!?;:'"()\-\n\r]+$/;
+    if (textarea.value === '') {
+        textarea.setCustomValidity('Por favor, rellena este campo.');
+    } else if (!regex.test(textarea.value)) {
+        textarea.setCustomValidity('El formato de la descripción es incorrecto.');
+    } else {
+        textarea.setCustomValidity(''); 
+    }
+}
