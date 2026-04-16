3acd2a34df63f1aeeefffe479ad1e6e10ee9fb1c
MiguelMoreno4 <143493511+MiguelMoreno4@users.noreply.github.com>
2026-04-16 20:11:50 +0200

    Pruebas de sistema CM-5

--- src/test/java/com/SYSTEM/SystemTest.java
+++ src/test/java/com/SYSTEM/SystemTest.java
@@ -48,4 +48,101 @@ class SystemTest {
 
         assertThat(exista).isTrue();
     }
+      // CM4-1: Tarea marcada como realizada con éxito
+    @Test
+    void flujoCompleto_completarTarea_ok() throws Exception {
+        Vivienda vivienda = new Vivienda();
+        vivienda.setName("CasaTest");
+        viviendaRepository.save(vivienda);
+
+
+        Roommate roommate = new Roommate("user1", "Juan", vivienda);
+        roommateRepository.save(roommate);
+
+
+        Tarea tarea = new Tarea();
+        tarea.setNombre("Limpiar baño");
+        tarea.setVivienda(vivienda);
+        tarea.setRoommate(roommate);
+        tarea.setCompletada(false);
+        tareaRepository.save(tarea);
+
+
+        mockMvc.perform(post("/tareas/" + tarea.getId() + "/completar")
+                .param("roommateId", String.valueOf(roommate.getId())))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/vivienda/" + vivienda.getId()));
+
+
+        boolean estaCompletada = tareaRepository.findById(tarea.getId()).get().isCompletada();
+       
+        assertThat(estaCompletada).isTrue();
+    }
+
+
+    // CM4-2: Error al marcar tarea que ya estaba realizada
+    @Test
+    void flujoCompleto_completarTarea_yaRealizada() throws Exception {
+        Vivienda vivienda = new Vivienda();
+        vivienda.setName("CasaTest2");
+        viviendaRepository.save(vivienda);
+
+
+        Roommate roommate = new Roommate("user2", "Maria", vivienda);
+        roommateRepository.save(roommate);
+
+
+        Tarea tarea = new Tarea();
+        tarea.setNombre("Fregar platos");
+        tarea.setVivienda(vivienda);
+        tarea.setRoommate(roommate);
+        tarea.setCompletada(true); // Ya está completada de antes
+        tareaRepository.save(tarea);
+
+
+        mockMvc.perform(post("/tareas/" + tarea.getId() + "/completar")
+                .param("roommateId", String.valueOf(roommate.getId())))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(flash().attributeExists("error"))
+                .andExpect(redirectedUrl("/vivienda/" + vivienda.getId()));
+
+
+        boolean sigueCompletada = tareaRepository.findById(tarea.getId()).get().isCompletada();
+       
+        assertThat(sigueCompletada).isTrue();
+    }
+
+
+    // CM4-3: Aviso al marcar tarea que no tiene roommate asignado
+    @Test
+    void flujoCompleto_completarTarea_sinAsignar() throws Exception {
+        Vivienda vivienda = new Vivienda();
+        vivienda.setName("CasaTest3");
+        viviendaRepository.save(vivienda);
+
+
+        Roommate roommate = new Roommate("user3", "Carlos", vivienda);
+        roommateRepository.save(roommate);
+
+
+        Tarea tarea = new Tarea();
+        tarea.setNombre("Bajar basura");
+        tarea.setVivienda(vivienda);
+        tarea.setRoommate(null); // Tarea sin nadie asignado
+        tarea.setCompletada(false);
+        tareaRepository.save(tarea);
+
+
+        mockMvc.perform(post("/tareas/" + tarea.getId() + "/completar")
+                .param("roommateId", String.valueOf(roommate.getId())))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(flash().attributeExists("error"))
+                .andExpect(redirectedUrl("/vivienda/" + vivienda.getId()));
+
+
+        boolean estaCompletada = tareaRepository.findById(tarea.getId()).get().isCompletada();
+       
+        assertThat(estaCompletada).isFalse();
+    }
+
 }
